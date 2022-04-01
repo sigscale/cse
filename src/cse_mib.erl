66b18037907e011988e4490719c86d76e76f176c
refath <refath@sigscale.com>
2022-04-01 06:52:53 +0000

    support getting local statistics

--- src/cse_mib.erl
+++ src/cse_mib.erl
@@ -109,6 +109,42 @@ dbp_local_config(get, Item) ->
 			{noValue, noSuchInstance}
 	end.
 
+-spec dbp_local_stats(Operation, Item) -> Result
+	when
+		Operation :: get,
+		Item :: uptime,
+		Result :: {value, Value} | {noValue, noSuchInstance} | genErr,
+		Value :: atom() | integer() | string() | [integer()].
+%% @doc Handle SNMP requests for `DIAMETER-BASE-PROTOCOL-MIB::dbpLocalStats'.
+%% @private
+dbp_local_stats(get, uptime) ->
+	case catch diameter_stats:uptime() of
+		{'EXIT', _Reason} ->
+			genErr;
+		{Hours, Mins, Secs, MicroSecs} ->
+			{value, (Hours * 360000) + (Mins * 6000)
+					+ (Secs * 100) + (MicroSecs div 10)}
+	end;
+dbp_local_stats(get, Item) ->
+	case lists:keyfind(cse_diameter_service, 1, diameter:services()) of
+		Service when is_tuple(Service) ->
+			case catch diameter:service_info(Service, transport) of
+				Info when is_list(Info) ->
+					case total_packets(Info) of
+						{ok, {PacketsIn, _}} when Item == in ->
+							{value, PacketsIn};
+						{ok, {_, PacketsOut}} when Item == out ->
+							{value, PacketsOut};
+						{error, not_found} ->
+							{noValue, noSuchInstance}
+					end;
+				_ ->
+					genErr
+			end;
+		false ->
+			genErr
+	end.
+
 %%----------------------------------------------------------------------
 %% internal functions
 %----------------------------------------------------------------------
@@ -116,4 +152,69 @@ dbp_local_config(get, Item) ->
 %% @hidden
 mibs() ->
 	["SIGSCALE-DIAMETER-BASE-PROTOCOL-MIB"].
+
+-spec total_packets(Info) -> Result
+	when
+		Info :: [tuple()],
+		Result :: {ok, {PacketsIn, PacketsOut}} | {error, Reason},
+		PacketsIn :: integer(),
+		PacketsOut :: integer(),
+		Reason :: term().
+%% @doc Get packet counts from service info.
+total_packets(Info) ->
+	total_packets(Info, {0, 0}).
+%% @hidden
+total_packets([H | T], Acc) ->
+	case lists:keyfind(accept, 1, H) of
+		{_, L} ->
+			case total_packets1(L, Acc) of
+				{ok, Acc1} ->
+					total_packets(T, Acc1);
+				{error, _Reason} ->
+					total_packets(T, Acc)
+			end;
+		false ->
+			{error, not_found}
+	end;
+total_packets([], Acc) ->
+	{ok, Acc}.
+%% @hidden
+total_packets1([H | T], Acc) ->
+	case lists:keyfind(port, 1, H) of
+		{_, L} ->
+			case total_packets2(L, Acc) of
+				{ok, Acc1} ->
+					total_packets1(T, Acc1);
+				{error, Reason} ->
+					{error, Reason}
+			end;
+		false ->
+			total_packets1(T, Acc)
+	end;
+total_packets1([], Acc) ->
+	{ok, Acc}.
+%% @hidden
+total_packets2(L1, Acc) ->
+	case lists:keyfind(statistics, 1, L1) of
+		{_, L2} ->
+			total_packets3(L2, Acc);
+		false ->
+			{error, not_found}
+	end.
+%% @hidden
+total_packets3(L, {PacketsIn, PacketsOut}) ->
+	case lists:keyfind(recv_cnt, 1, L) of
+		{_, N} ->
+			total_packets4(L, {PacketsIn + N, PacketsOut});
+		false ->
+			{error, not_found}
+	end.
+%% @hidden
+total_packets4(L, {PacketsIn, PacketsOut}) ->
+	case lists:keyfind(send_cnt, 1, L) of
+		{_, N} ->
+			{ok, {PacketsIn, PacketsOut + N}};
+		false ->
+			{error, not_found}
+	end.
 
