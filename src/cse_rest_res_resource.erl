019e03f242f67bbe38203be686daba36c51e53af
Vance Shipley <vances@sigscale.org>
2023-01-13 07:50:34 +0800

    accept http requests for problem detail content (rfc7807)

--- src/cse_rest_res_resource.erl
+++ src/cse_rest_res_resource.erl
@@ -60,7 +60,7 @@ content_types_accepted() ->
 		ContentTypes :: list().
 %% @doc Provides list of resource representations available.
 content_types_provided() ->
-	["application/json"].
+	["application/json", "application/problem+json"].
 
 -spec get_resource_spec(ID) -> Result
 	when
