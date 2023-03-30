8f3dd6231acbab546b0645b4427403fbf5cb3a6e
Vance Shipley <vances@sigscale.org>
2023-03-30 12:26:44 +0000

    gtt test cases for insert, lookup and disk only

--- test/cse_gtt_SUITE.erl
+++ test/cse_gtt_SUITE.erl
@@ -29,9 +29,15 @@
 %% test cases
 -export([add_table/0, add_table/1,
 		delete_table/0, delete_table/1,
+		insert/0, insert/1,
+		lookup/0, lookup/1,
 		range/0, range/1,
 		add_range/0, add_range/1,
-		delete_range/0, delete_range/1]).
+		delete_range/0, delete_range/1,
+		big_table/0, big_table/1,
+		big_insert/0, big_insert/1,
+		big_lookup/0, big_lookup/1,
+		big_range/0, big_range/1]).
 
 -include("cse.hrl").
 -include_lib("common_test/include/ct.hrl").
@@ -70,11 +76,20 @@ end_per_suite(Config) ->
 -spec init_per_testcase(TestCase :: atom(), Config :: [tuple()]) -> Config :: [tuple()].
 %% Initialization before each test case.
 %%
-init_per_testcase(add_table = _TestCase, Config) ->
+init_per_testcase(TestCase, Config)
+		when TestCase == add_table;
+		TestCase == big_table ->
 	Table = list_to_atom(cse_test_lib:rand_name()),
 	lists:keystore(table, 1, Config, {table, Table});
 init_per_testcase(range = _TestCase, Config) ->
 	Config;
+init_per_testcase(TestCase, Config)
+		when TestCase == big_insert;
+		TestCase == big_lookup;
+		TestCase == big_range ->
+	Table = list_to_atom(cse_test_lib:rand_name()),
+	ok = cse_gtt:new(Table, [{disc_only_copies, [node()]}]),
+	lists:keystore(table, 1, Config, {table, Table});
 init_per_testcase(_TestCase, Config) ->
 	Table = list_to_atom(cse_test_lib:rand_name()),
 	ok = cse_gtt:new(Table, []),
@@ -101,7 +116,8 @@ sequences() ->
 %% Returns a list of all test cases in this test suite.
 %%
 all() ->
-	[add_table, delete_table, range, add_range, delete_range].
+	[add_table, delete_table, insert, lookup, range, add_range,
+			delete_range, big_table, big_insert, big_lookup, big_range].
 
 %%---------------------------------------------------------------------
 %%  Test cases
@@ -112,7 +128,9 @@ add_table() ->
 
 add_table(Config) ->
 	Table = ?config(table, Config),
-	ok = cse_gtt:new(Table, []).
+	ok = cse_gtt:new(Table, []),
+	gtt = mnesia:table_info(Table, record_name),
+	disc_copies = mnesia:table_info(Table, storage_type).
 
 delete_table() ->
 	[{userdata, [{doc, "Delete a prefix table."}]}].
@@ -120,6 +138,20 @@ delete_table() ->
 delete_table(Config) ->
 	Table = ?config(table, Config),
 	ok = cse_gtt:delete(Table).
+
+insert() ->
+	[{userdata, [{doc, "Add a key and value to prefix table"}]}].
+
+insert(Config) ->
+	Table = ?config(table, Config),
+	do_insert(Table).
+
+lookup() ->
+	[{userdata, [{doc, "Find a key and value in prefix table"}]}].
+
+lookup(Config) ->
+	Table = ?config(table, Config),
+	do_lookup(Table).
 
 range() ->
 	[{userdata, [{doc, "Prefixes for range"}]}].
@@ -161,9 +193,71 @@ delete_range(Config) ->
 	ok = cse_gtt:delete_range(Table, Start, End),
 	F1 = fun(#gtt{value = undefined}, Acc) ->
 				Acc;
-			(#gtt{}, Acc) ->
+			(#gtt{}, _Acc) ->
 				mnesia:abort(garbage)
 	end,
 	F2 = fun() -> mnesia:foldl(F1, 0, Table) end,
 	{atomic, 0} = mnesia:transaction(F2).
+
+big_table() ->
+	[{userdata, [{doc, "Add a disk only prefix table"}]}].
+
+big_table(Config) ->
+	Table = ?config(table, Config),
+	ok = cse_gtt:new(Table, [{disc_only_copies, [node()]}]),
+	gtt = mnesia:table_info(Table, record_name),
+	disc_only_copies = mnesia:table_info(Table, storage_type).
+
+big_insert() ->
+	[{userdata, [{doc, "Add a key and value to disk only prefix table"}]}].
+
+big_insert(Config) ->
+	Table = ?config(table, Config),
+	do_insert(Table).
+
+big_lookup() ->
+	[{userdata, [{doc, "Find a key and value in disk only prefix table"}]}].
+
+big_lookup(Config) ->
+	Table = ?config(table, Config),
+	do_lookup(Table).
+
+big_range() ->
+	[{userdata, [{doc, "Add prefixes to a disk only table by range"}]}].
+
+big_range(Config) ->
+	Table = ?config(table, Config),
+	Digits1 = io_lib:fwrite("~3.10.0b", [rand:uniform(1000) - 1]),
+	Digits2 = io_lib:fwrite("~3.10.0b", [rand:uniform(1000) - 1]),
+	[Start, End] = lists:sort([Digits1, Digits2]),
+	Value = make_ref(),
+	ok = cse_gtt:add_range(Table, Start, End, Value),
+	disc_only_copies = mnesia:table_info(Table, storage_type).
+
+%%---------------------------------------------------------------------
+%%  Internal functions
+%%---------------------------------------------------------------------
+
+do_insert(Table) ->
+	Digits = io_lib:fwrite("~3.10.0b", [rand:uniform(1000) - 1]),
+	Value = make_ref(),
+	{ok, #gtt{}} = cse_gtt:insert(Table, Digits, Value).
+
+do_lookup(Table) ->
+	F = fun F(N, Acc) when N > 0 ->
+				D = io_lib:fwrite("~7.10.0b",
+						[rand:uniform(10000000) - 1]),
+				V = make_ref(),
+				NewAcc = lists:keystore(D, 1, Acc, {D, V}), 
+				F(N - 1, NewAcc);
+			F(0, Acc) ->
+				Acc
+	end,
+	Items = F(1000, []),
+	ok = cse_gtt:insert(Table, Items),
+	Index = rand:uniform(length(Items)),
+	{Prefix, Value} = lists:nth(Index, Items),
+	Address = Prefix ++ io_lib:fwrite("~3.10.0b",
+			[rand:uniform(1000) - 1]),
+	Value = cse_gtt:lookup_last(Table, Address).
 
