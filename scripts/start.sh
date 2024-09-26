75a1fd2dfd376438b9898da8588f685e584f95e1
Vance Shipley <vances@sigscale.org>
2024-09-26 13:48:37 +0000

    Revert "refactor systemd service with start script"
    This reverts commit de648591e4f663b70dd99a54b98c7c84a8d9d567.

--- scripts/start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-${ROOTDIR}/bin/run_erl -daemon /run/${NODENAME}/ log \
-		"ERL_LIBS=lib exec ${ROOTDIR}/bin/start_erl \
-				${ROOTDIR} ${RELDIR} ${START_ERL_DATA} 
-				-boot_var OTPHOME . \
-				+K true +A 32 +Bi \
-				-sname ${NODENAME}"
-
