93d47092dbe3cd5b24db9984b237c4b7cc996044
Vance Shipley <vances@sigscale.org>
2024-09-26 01:56:00 +0000

    refactor exception handling in install release script

--- scripts/install_release.sh
+++ scripts/install_release.sh
@@ -3,7 +3,6 @@
 set -x
 PKG_NAME=cse
 
-set -e
 cd ${HOME}
 if [ -f "releases/RELEASES" ];
 then
@@ -28,27 +27,24 @@ then
 	cp releases/$PKG_NEW/sys.config releases/$PKG_NEW/sys.config.dist
 	if [ -f "releases/$PKG_NAME-$OLD_VER/sys.config.dist" ];
 	then
-		set +e
 		echo "Merging previous system configuration localizations (sys.config) ..."
 		diff --text --unified releases/$PKG_NAME-$OLD_VER/sys.config.dist \
 				releases/$PKG_NAME-$OLD_VER/sys.config \
-				> releases/$PKG_NAME-$OLD_VER/sys.config.patch
-		patch releases/$PKG_NEW/sys.config \
-				releases/$PKG_NAME-$OLD_VER/sys.config.patch && echo "... merge done."
-		if [ "$?" -ne 0 ];
+				> releases/$PKG_NAME-$OLD_VER/sys.config.patch || :
+		if patch releases/$PKG_NEW/sys.config releases/$PKG_NAME-$OLD_VER/sys.config.patch;
 		then
+			echo "... merge done."
+		else
 			echo "... merge failed."
 			echo "Using previous system configuration without any newly distributed changes."
 			sed -e "s/$PKG_NAME-$OLD_VER/$PKG_NEW/" \
 					releases/$PKG_NAME-$OLD_VER/sys.config > releases/$PKG_NEW/sys.config
 		fi
-		set -e
 	else
 		echo "Using previous system configuration localizations (sys.config) without any newly distributed changes."
 		sed -e "s/$PKG_NAME-$OLD_VER/$PKG_NEW/" \
 				releases/$PKG_NAME-$OLD_VER/sys.config > releases/$PKG_NEW/sys.config
 	fi
-	set +e
 	if epmd -names 2> /dev/null | grep -q '^name cse at';
 	then
 		# Upgrade using rpc
@@ -56,31 +52,40 @@ then
 		echo "Performing an in-service upgrade ..."
 		if echo -e "4.2\n$SASLVER"  | sort --check=quiet --version-sort;
 		then
-			set -e
-			erl -noshell -sname $RPC_SNAME \
+			if erl -noshell -sname $RPC_SNAME \
 					-eval "rpc:call('$OTP_NODE', application, start, [sasl])" \
 					-eval "rpc:call('$OTP_NODE', systools, make_relup, [\"releases/$PKG_NEW\", [\"releases/$PKG_NAME-$OLD_VER\"], [\"releases/$PKG_NAME-$OLD_VER\"], [{path,[\"lib/$PKG_NEW/ebin\"]}, {outdir, \"releases/$PKG_NEW\"}]])" \
 					-eval "{ok, _} = rpc:call('$OTP_NODE', release_handler, set_unpacked, [\"$HOME/releases/$PKG_NEW.rel\", $APPDIRS])" \
 					-eval "{ok, _, _} = rpc:call('$OTP_NODE', release_handler, install_release, [\"$PKG_NEW\", [{update_paths, true}]])" \
 					-eval "ok = rpc:call('$OTP_NODE', release_handler, make_permanent, [\"$PKG_NEW\"])" \
-					-s init stop && echo "... done."
+					-s init stop;
+			then
+				echo "... done."
+			else
+				echo "... failed."
+				exit 1
+			fi
 		else
-			set -e
-			erl -noshell -sname $RPC_SNAME \
+			if erl -noshell -sname $RPC_SNAME \
 					-eval "rpc:call('$OTP_NODE', application, start, [sasl])" \
 					-eval "rpc:call('$OTP_NODE', systools, make_relup, [\"releases/$PKG_NEW\", [\"releases/$PKG_NAME-$OLD_VER\"], [\"releases/$PKG_NAME-$OLD_VER\"], [{path,[\"lib/$PKG_NEW/ebin\"]}, {outdir, \"releases/$PKG_NEW\"}]])" \
 					-eval "{ok, _} = rpc:call('$OTP_NODE', release_handler, set_unpacked, [\"releases/$PKG_NEW.rel\", $APPDIRS])" \
 					-eval "{ok, _, _} = rpc:call('$OTP_NODE', release_handler, install_release, [\"$PKG_NEW\", [{update_paths, true}]])" \
 					-eval "ok = rpc:call('$OTP_NODE', release_handler, make_permanent, [\"$PKG_NEW\"])" \
-					-s init stop && echo "... done."
+					-s init stop;
+			then
+				echo "... done."
+			else
+				echo "... failed."
+				exit 1
+			fi
 		fi
 	else
 		# Start sasl and mnesia, perform upgrade, stop started applications
 		echo "Performing an out-of-service upgrade ..."
 		if echo -e "4.2\n$SASLVER"  | sort --check=quiet --version-sort;
 		then
-			set -e
-			ERL_LIBS=lib RELDIR=$HOME/releases erl -noshell \
+			if ERL_LIBS=lib RELDIR=$HOME/releases erl -noshell \
 					-sname $OTP_NODE -config releases/$PKG_NAME-$OLD_VER/sys \
 					-s mnesia \
 					-eval "application:start(sasl)" \
@@ -89,10 +94,15 @@ then
 					-eval "{ok, _} = release_handler:set_unpacked(\"$HOME/releases/$PKG_NEW.rel\", $APPDIRS)" \
 					-eval "{ok, _, _} = release_handler:install_release(\"$PKG_NEW\", [{update_paths, true}])" \
 					-eval "ok = release_handler:make_permanent(\"$PKG_NEW\")" \
-					-s init stop && echo "... done."
+					-s init stop;
+			then
+				echo "... done."
+			else
+				echo "... failed."
+				exit 1
+			fi
 		else
-			set -e
-			ERL_LIBS=lib RELDIR=releases erl -noshell -sname $OTP_NODE -config releases/$PKG_NAME-$OLD_VER/sys \
+			if ERL_LIBS=lib RELDIR=releases erl -noshell -sname $OTP_NODE -config releases/$PKG_NAME-$OLD_VER/sys \
 					-s mnesia \
 					-eval "application:start(sasl)" \
 					-eval "application:load($PKG_NAME)" \
@@ -100,24 +110,39 @@ then
 					-eval "{ok, _} = release_handler:set_unpacked(\"releases/$PKG_NEW.rel\", $APPDIRS)" \
 					-eval "{ok, _, _} = release_handler:install_release(\"$PKG_NEW\", [{update_paths, true}])" \
 					-eval "ok = release_handler:make_permanent(\"$PKG_NEW\")" \
-					-s init stop && echo "... done."
+					-s init stop;
+			then
+				echo "... done."
+			else
+				echo "... failed."
+				exit 1
+			fi
 		fi
 	fi
 else
 	# Install release via shell
 	echo "Installing an initial release ..."
-	set +e
 	if echo -e "4.2\n$SASLVER"  | sort --check=quiet --version-sort;
 	then
-		set -e
-		RELDIR=$HOME/releases erl -noshell -eval "application:start(sasl)" \
+		if RELDIR=$HOME/releases erl -noshell -eval "application:start(sasl)" \
 				-eval "ok = release_handler:create_RELEASES(\"$HOME/releases\", \"$HOME/releases/$PKG_NEW.rel\", $APPDIRS)" \
-				-s init stop && echo "... done."
+				-s init stop;
+		then
+			echo "... done."
+		else
+			echo "... failed."
+			exit 1
+		fi
 	else
-		set -e
-		RELDIR=releases erl -noshell -eval "application:start(sasl)" \
+		if RELDIR=releases erl -noshell -eval "application:start(sasl)" \
 				-eval "ok = release_handler:create_RELEASES(code:root_dir(), \"$HOME/releases\", \"$HOME/releases/$PKG_NEW.rel\", $APPDIRS)" \
-				-s init stop && echo "... done."
+				-s init stop;
+		then
+			echo "... done."
+		else
+			echo "... failed."
+			exit 1
+		fi
 	fi
 	if ! test -f releases/RELEASES;
 	then
