4dfb7b2e9830b9e706cd0395c485a6142c0e8b8b
Vance Shipley <vances@sigscale.org>
2025-05-06 11:02:10 +0800

    typo in timeout macro (ignored)

--- src/cse_slp_prepaid_diameter_ims_fsm.erl
+++ src/cse_slp_prepaid_diameter_ims_fsm.erl
@@ -104,7 +104,7 @@
 -define(SERVICENAME, "Prepaid Voice").
 -define(FSM_LOGNAME, prepaid).
 -define(NRF_LOGNAME, rating).
--define(IDLE_TIMEOUT(Data), {timeout, maps:get(idle, Data), idle}).
+-define(IDLE_TIMEOUT(Data), {timeout, maps:get(idle, Data), infinity}).
 
 -type state() :: null
 		| authorize_origination_attempt | terminating_call_handling
