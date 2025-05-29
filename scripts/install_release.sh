fa9b8797f03c6aa06166c8aeed493e6298f680c3
Vance Shipley <vances@sigscale.org>
2025-05-29 09:45:46 +0800

    debug flag in install_release script

--- scripts/install_release.sh
+++ scripts/install_release.sh
@@ -1,15 +1,34 @@
 #!/bin/bash
+########################################################################
+# Copyright (c) 2018 - 2025 SigScale Global Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+########################################################################
 # Install an Erlang/OTP release package
 
 usage() {
-	echo "usage: $0 [-p] APP_NAME"
+	echo "usage: $0 [-d] [-p] APP_NAME"
 	exit 1;
 }
 
-while getopts ":p" opt; do
+while getopts ":pd" opt; do
   case ${opt} in
     p)
       PERMANENT=true
+      ;;
+    d)
+      echo "$0 $*"
+		set -x
       ;;
     ?)
       usage
